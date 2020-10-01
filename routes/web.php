aff5fba12b319148269aef720900b1124d33cb9e
hodev-dev <hodev.dev@gmail.com>
2020-10-01 21:38:46 +0330

    add authentication

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,22 @@
 <?php
 
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::post('/login', function (Request  $request) {
+    $credentials = $request->only('email', 'password');
+    if (Auth::attempt($credentials)) {
+        return response()->json(["auth" => Auth::check()]);
+    } else {
+        return response()->json(["auth" => Auth::check()]);
+    }
+});
+
+Route::get('/logout', function (Request  $request) {
+    Auth::logout();
 });
